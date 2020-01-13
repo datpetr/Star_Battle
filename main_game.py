0d1fc0925a74a1e686cc958295b2fd031bf6c57b
datpetr <57681066+datpetr@users.noreply.github.com>
2020-01-13 23:05:08 +0300

    BASE_GAME
    base of future game

---
+++ main_game.py
@@ -0,0 +1,178 @@
+# Игра Shmup - 4 часть
+# Графика
+import pygame
+import random
+from os import path
+
+
+def load_image(name, colorkey=None):
+    fullname = path.join('data', name)
+    if colorkey is not None:
+        image = pygame.image.load(fullname).convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        # Оставляем картинку прозрачной
+        image = pygame.image.load(fullname).convert_alpha()
+    return image
+
+
+img_dir = path.join(path.dirname(__file__), 'data')
+
+size = WIDTH, HEIGHT = [1536, 864]
+screen = pygame.display.set_mode(size)
+FPS = 60
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+colors = [(255, 255, 255), (0, 0, 255), (30, 144, 255), (255, 69, 0), (255, 255, 0)]
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+pygame.display.set_caption("STAR BATTLE")
+clock = pygame.time.Clock()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = meteor_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        # убить, если он заходит за верхнюю часть экрана
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+# Загрузка всей игровой графики
+player_img = load_image("falcon.png")
+meteor_img = load_image("meteor.png")
+bullet_img = load_image("laser.png")
+
+all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+star_list = []
+
+for i in range(15):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+# Добавляем 1000 звезд со случайными координатами
+for i in range(1000):
+    x = random.randrange(0, 1536)
+    y = random.randrange(0, 1536)
+    star_list.append([x, y, 2])
+clock = pygame.time.Clock()
+
+
+# Цикл игры
+running = True
+while running:
+    # Ввод процесса (события)
+    screen.fill(BLACK)
+    for event in pygame.event.get():
+        # проверка для закрытия окна
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+
+    # Обновление
+    all_sprites.update()
+    for star in star_list:
+        # Рисуем звезду
+        number = random.randint(0, 4)
+        pygame.draw.circle(screen, colors[number], star[0:2], 2)
+
+        # Смещаем звезду вниз
+        star[1] += star[2]
+
+        # Если звезда упала за низ окна
+        if star[1] > 1536:
+            # Устанавливаем для нее новые случайные координаты (конечноже выше экрана)
+            star[0] = random.randrange(0, 1536)
+            star[1] = random.randrange(-50, -10)
+
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+
+    # Проверка, не ударил ли метеор игрока
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        running = False
+
+    # Выводим на экран все что нарисовали
+    all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+    clock.tick(FPS)
+pygame.quit()

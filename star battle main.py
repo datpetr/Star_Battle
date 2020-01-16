649fb74ecc64cf1818fb4c1fe2a188a9a3b071c3
datpetr <57681066+datpetr@users.noreply.github.com>
2020-01-16 21:39:56 +0300

    Delete star battle main.py

--- star battle main.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import os
-import sys
-import pkgutil
-
-
-search_path = ['.'] # Используем None, чтобы увидеть все модули, импортируемые из sys.path
-all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
-print(all_modules)
